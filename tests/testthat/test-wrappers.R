b2771ea8f9d8177ae5311e8bf5ce80bc76d2f031
John Rothen <rackjothen@gmail.com>
2025-08-16 11:36:49 -0400

    started on tests

---
+++ tests/testthat/test-wrappers.R
@@ -0,0 +1,12 @@
+test_that("multiplication works", {
+  # expect_equal(derlang(1,1,1), 0.3678794)
+  # expect_equal(derlang_c(1,1,1), 0.3678794)
+  # expect_equal(derlang(c(1,3,4,5),c(1,2),1), c(0.36787944, 0.14936121, 0.01831564, 0.03368973))
+  #
+  # expect_equal(dgamgomp(1,1,1,1), 0.3678794)
+  # expect_equal(dgamgomp_c(1,1,1,1), 0.3678794)
+  # expect_equal(dgamgomp(c(1,2,3,4),1,c(2,3),1), c(2.706706e-01, 7.436257e-03, 4.957504e-03, 1.843264e-05))
+  #
+  # expect_equal(dhypertab(1,1,1), 0.1701686)
+  # expect_equal(dhypertab_c(1,1,1), 0.1701686)
+})
