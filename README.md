6cb96d169f0967ed1008f6aeeb35a82f8e91aeec
John Rothen <rackjothen@gmail.com>
2025-09-19 21:43:33 -0400

    trimming

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-This package is designed to provide a simple, one-line-of-code approach to testing parametric survival functions on a set of data. 
+This package is designed to provide a simple, one-line-of-code approach to testing a variety of parametric survival distributions on a data set. 
 This is done by running 60+ different parametric survival curves in order to see which one may be best suited to describing your data! 
 
 ## Installation
@@ -24,11 +24,11 @@ library(survshotgunR)
 
 # sample dataset available in <survival>
 library(survival)
-surv_shotgun(Surv(time, status)~1, data=aml, dump_models=TRUE) 
+surv_shotgun(Surv(time, status)~1, data=aml, dump_models = TRUE)
 
 ```
 
 
 ## Developer's Notes
 
-This is still a very young package, so please expect some errors to occur. Please feel free to submit issues via github if you find them!
+This is still a very young package, so please expect some errors to occur. Please feel free to submit issues via github if you find them.
--- tests/testthat/test-shotgun.R
+++ tests/testthat/test-shotgun.R
@@ -1,1 +1,19 @@
-# TBD
+### raw testing chunk from legacy testing
+# if(F){
+#
+#   ###
+#   # coxph(survival::Surv(time, status) ~ 1, data = survival::aml)
+#   # coxph(survival::Surv(time, status) ~ factor(x), data = survival::aml)
+#
+#   ### Verify flexsurvreg and survreg work the same for us (same LL, AIC, etc)
+#   # survreg(survival::Surv(time, status) ~ 1, data = survival::aml, dist = 'exp')
+#   # flexsurvreg(survival::Surv(time, status) ~ 1, data = survival::aml, dist = 'exp')
+#
+#   # simple model tests
+#   surv_shotgun(survival::Surv(time, status) ~ 1, data = survival::aml,    dump_models = T, warn = F, spline=c('rp','wy'), detailed = T, ibs=T) -> test_models
+#   surv_shotgun(survival::Surv(time, status) ~ 1, data = survival::cancer, dump_models = T, warn = F) -> test_models2
+#
+#   # single variable models
+#   surv_shotgun(survival::Surv(time, status) ~ x,   data = survival::aml,    dump_models = T, warn = F) -> test_models3
+#   surv_shotgun(survival::Surv(time, status) ~ sex, data = survival::cancer, dump_models = T, warn = F) -> test_models4
+# }
